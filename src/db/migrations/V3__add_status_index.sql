ee644638761c27846ea474c39e6553369499f925
Alexey Plahuta <pat2-2@yandex.ru>
2026-03-17 20:30:57 +0300

    init commit

--- src/db/migrations/V3__add_status_index.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_products_status ON products(status);
--- src/server/context.py
+++
@@ -1,4 +0,0 @@
-from contextvars import ContextVar
-
-current_user_id: ContextVar[str | None] = ContextVar("current_user_id", default=None)
-current_role: ContextVar[str | None] = ContextVar("current_role", default=None)
